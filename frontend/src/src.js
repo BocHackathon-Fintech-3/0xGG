f3ea2193421f64b99873a9ca4c7d40e7c0c48334
Nicolas Tsagarides <nicexe@gmail.com>
2019-10-20 15:20:36 +0300

    fix web3 integration

--- frontend/src/src.js
+++ frontend/src/src.js
@@ -1,8 +1,8 @@
 // function init() {
-var loanFactoryAddress = "0x83a2f2e6b49BcD08a772ba88B0A2C42dba317f75"
+var loanFactoryAddress = "0xdE675837Efd12621e60e16c64a8C3AA29a5D33e2"
 var loanFactoryAbi = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":true,"internalType":"address","name":"instantiation","type":"address"}],"name":"ContractInstantiation","type":"event"},{"constant":false,"inputs":[{"internalType":"uint256","name":"_principal_units","type":"uint256"},{"internalType":"bytes32","name":"_principal_symbol","type":"bytes32"},{"internalType":"uint256","name":"_collateral_units","type":"uint256"},{"internalType":"bytes32","name":"_collateral_symbol","type":"bytes32"},{"internalType":"uint64","name":"_deadline","type":"uint64"}],"name":"createLoan","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract FundHandler","name":"_fundHandler","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"created","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"fundHandler","outputs":[{"internalType":"contract FundHandler","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"addr","type":"address"}],"name":"getIsLoan","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"isLoan","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}];
 
-var loanAbi = [{"anonymous":false,"inputs":[],"name":"Failure","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"lender","type":"address"},{"indexed":false,"internalType":"uint256","name":"interest","type":"uint256"}],"name":"NewOffer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"lender","type":"address"},{"indexed":false,"internalType":"uint256","name":"interest","type":"uint256"}],"name":"OfferAgreed","type":"event"},{"anonymous":false,"inputs":[],"name":"Success","type":"event"},{"constant":false,"inputs":[],"name":"agree","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"collectCollateral","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"_interest","type":"uint256"}],"name":"matchLoan","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"repay","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract FundHandler","name":"_fundHandler","type":"address"},{"internalType":"address","name":"_loaner","type":"address"},{"internalType":"uint256","name":"_principal_units","type":"uint256"},{"internalType":"bytes32","name":"_principal_symbol","type":"bytes32"},{"internalType":"uint256","name":"_collateral_units","type":"uint256"},{"internalType":"bytes32","name":"_collateral_symbol","type":"bytes32"},{"internalType":"uint64","name":"_deadline","type":"uint64"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[],"name":"fundHandler","outputs":[{"internalType":"contract FundHandler","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"offer","outputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"interest","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"state","outputs":[{"internalType":"enum Loan.States","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"}];
+var loanAbi = [{"anonymous":false,"inputs":[],"name":"Failure","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"lender","type":"address"},{"indexed":false,"internalType":"uint256","name":"interest","type":"uint256"}],"name":"NewOffer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"lender","type":"address"},{"indexed":false,"internalType":"uint256","name":"interest","type":"uint256"}],"name":"OfferAgreed","type":"event"},{"anonymous":false,"inputs":[],"name":"Success","type":"event"},{"constant":false,"inputs":[],"name":"agree","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"collectCollateral","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"_interest","type":"uint256"}],"name":"matchLoan","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"repay","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"contract FundHandler","name":"_fundHandler","type":"address"},{"internalType":"address","name":"_loaner","type":"address"},{"internalType":"uint256","name":"_principal_units","type":"uint256"},{"internalType":"bytes32","name":"_principal_symbol","type":"bytes32"},{"internalType":"uint256","name":"_collateral_units","type":"uint256"},{"internalType":"bytes32","name":"_collateral_symbol","type":"bytes32"},{"internalType":"uint64","name":"_deadline","type":"uint64"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[],"name":"collateral_symbol","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"collateral_units","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"deadline","outputs":[{"internalType":"uint64","name":"","type":"uint64"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"fundHandler","outputs":[{"internalType":"contract FundHandler","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"offer","outputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"interest","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"principal_symbol","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"principal_units","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"state","outputs":[{"internalType":"enum Loan.States","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"}];
 
 
 const promisify = (inner) =>
@@ -55,9 +55,9 @@ async function getLoanInfo(address, temp) {
 }
 
 async function loadLoans() {
-  var factoryContract = web3.eth.contract(factoryAbi);
+  var factoryContract = web3.eth.contract(loanFactoryAbi);
   // instantiate by address
-  var factory = factoryContract.at(factoryAddress);
+  var factory = factoryContract.at(loanFactoryAddress);
   log = await promisify(cb => factory.ContractInstantiation({}, {fromBlock: 0, toBlock: 'latest'}).get(cb));
   var result = []
   for (var i in log) {
@@ -69,10 +69,12 @@ async function loadLoans() {
   return result
 }
 
-async function createLoan(owner, principal_units, principal_symbol, collateral_units, collateral_symbol, deadline) {
-  var factoryContract = web3.eth.contract(factoryAbi);
+async function createLoan(principal_units, principal_symbol, collateral_units, collateral_symbol, deadline) {
+    
+    owner = web3.eth.accounts[0];
+  var factoryContract = web3.eth.contract(loanFactoryAbi);
   // instantiate by address
-  var factory = factoryContract.at(factoryAddress);
+  var factory = factoryContract.at(loanFactoryAddress);
   console.log(principal_units);
   console.log(principal_symbol);
   console.log(collateral_units);
@@ -115,13 +117,15 @@ async function createLoan(owner, principal_units, principal_symbol, collateral_u
   })
 }
 
-async function makeOffer(owner, interest) {
-  var factoryContract = web3.eth.contract(factoryAbi);
+async function makeOffer(loanAddress, interest) {
+    
+    owner = web3.eth.accounts[0];
+  var loanContract = web3.eth.contract(loanAbi);
   // instantiate by address
-  var factory = factoryContract.at(factoryAddress);
+  var loan = loanContract.at(loanAddress);
   console.log(interest);
   promisify(cb => {
-    return factory.matchLoan.call(
+    return loan.matchLoan.call(
       interest,
       {
         from: owner,
@@ -131,7 +135,7 @@ async function makeOffer(owner, interest) {
     )
   })
   .then(function (address) {
-    promisify(cb => {return factory.matchLoan(
+    promisify(cb => {return loan.matchLoan(
       interest,
       {
         from: owner,
@@ -149,12 +153,13 @@ async function makeOffer(owner, interest) {
   })
 }
 
-async function agree(owner) {
-  var factoryContract = web3.eth.contract(factoryAbi);
+async function agree(loanAddress) {
+    owner = web3.eth.accounts[0];
+    var loanContract = web3.eth.contract(loanAbi);
   // instantiate by address
-  var factory = factoryContract.at(factoryAddress);
+  var loan = loanContract.at(loanAddress);
   promisify(cb => {
-    return factory.agree.call(
+    return loan.agree.call(
       {
         from: owner,
         gasPrice: 22000000000
@@ -162,7 +167,7 @@ async function agree(owner) {
     )
   })
   .then(function (address) {
-    promisify(cb => {return agree.createLoan(
+    promisify(cb => {return loan.agree(
       {
         from: owner,
         // value: 1,
@@ -179,12 +184,13 @@ async function agree(owner) {
   })
 }
 
-async function repay(owner) {
-  var factoryContract = web3.eth.contract(factoryAbi);
+async function repay() {
+    owner = web3.eth.accounts[0];
+    var loanContract = web3.eth.contract(loanAbi);
   // instantiate by address
-  var factory = factoryContract.at(factoryAddress);
+  var loan = loanContract.at(loanAddress);
   promisify(cb => {
-    return factory.repay.call(
+    return loan.repay.call(
       {
         from: owner,
         gasPrice: 22000000000
@@ -192,7 +198,7 @@ async function repay(owner) {
     )
   })
   .then(function (address) {
-    promisify(cb => {return agree.repay(
+    promisify(cb => {return loan.repay(
       {
         from: owner,
         // value: 1,
@@ -209,12 +215,13 @@ async function repay(owner) {
   })
 }
 
-async function repay(owner) {
-  var factoryContract = web3.eth.contract(factoryAbi);
+async function collectCollateral() {
+    owner = web3.eth.accounts[0];
+var loanContract = web3.eth.contract(loanAbi);
   // instantiate by address
-  var factory = factoryContract.at(factoryAddress);
+  var loan = loanContract.at(loanAddress);
   promisify(cb => {
-    return factory.collectCollateral.call(
+    return loan.collectCollateral.call(
       {
         from: owner,
         gasPrice: 22000000000
@@ -222,7 +229,7 @@ async function repay(owner) {
     )
   })
   .then(function (address) {
-    promisify(cb => {return agree.collectCollateral(
+    promisify(cb => {return loan.collectCollateral(
       {
         from: owner,
         // value: 1,
